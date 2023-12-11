2ab9824e153fe5c36ad00c38ae438eb76862765e
Oscar <info@oscargallegoruiz.com>
2023-12-11 17:02:56 +0100

    change phone size

--- app/components/Phone.tsx
+++ app/components/Phone.tsx
@@ -7,7 +7,7 @@ export default function Phone({ children }: PhoneProps) {
 		<div className="mockup-phone border-primar">
 			<div className="camera"></div>
 			<div className="display ">
-				<div className="artboard artboard-demo phone-5">{children}</div>
+				<div className="artboard artboard-demo phone-3">{children}</div>
 			</div>
 		</div>
 	)
