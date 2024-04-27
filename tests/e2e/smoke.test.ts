3f81486bc07e4fcd2c170092d368f176052673f7
Óscar Gallego <oscar.gallego@checkinscan.com>
2024-04-27 12:06:24 +0200

    Add some tests to pipline

--- tests/e2e/smoke.test.ts
+++ tests/e2e/smoke.test.ts
@@ -2,5 +2,24 @@ import { test, expect } from '@playwright/test'
 
 test('can visit the home page', async ({ page }) => {
 	await page.goto('/')
-	await expect(page.getByText('Hello World')).toBeVisible()
+	await expect(
+		page.getByRole('heading', {
+			name: /bazar online/i,
+		}),
+	).toBeVisible()
+})
+
+test('There is a Search button in home page', async ({ page }) => {
+	await page.goto('/')
+	await page.screenshot()
+	await expect(
+		page.getByRole('button', {
+			name: /search/i,
+		}),
+	).toContainText('Search')
+})
+
+test('There is a Search input in home page', async ({ page }) => {
+	await page.goto('/')
+	await expect(page.getByRole('searchbox')).toBeVisible()
 })
